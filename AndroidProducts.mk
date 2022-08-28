4353f4bd2149a8252fea9679ba7b6553ce78f293
cyberdroid1111 <cyberdroid132@gmail.com>
2022-08-28 09:23:59 +0000

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/corvus_RMX3360.mk
+
+COMMON_LUNCH_CHOICES := \
+    corvus_RMX3360-user \
+    corvus_RMX3360-userdebug \
+    corvus_RMX3360-eng
